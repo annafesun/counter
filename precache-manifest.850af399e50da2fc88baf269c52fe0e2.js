ac1856a268854aec064ead33b281ae9b32a4949b
annafesun <aniutafes@gmail.com>
2021-11-28 19:13:03 +0200

    Updates

---
+++ precache-manifest.850af399e50da2fc88baf269c52fe0e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0820f91103d9467caa254e49119b566",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "318e2dfca7afd4e2a6b5",
+    "url": "/counter/static/css/main.39a93508.chunk.css"
+  },
+  {
+    "revision": "2af82fec14a37515af24",
+    "url": "/counter/static/js/2.62419dd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter/static/js/2.62419dd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "318e2dfca7afd4e2a6b5",
+    "url": "/counter/static/js/main.3b647916.chunk.js"
+  },
+  {
+    "revision": "095e72424f5483516c4f",
+    "url": "/counter/static/js/runtime-main.5de7866a.js"
+  }
+]);
